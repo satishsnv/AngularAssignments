51f61edc7d0fe268f21da645e18e4fa2daa27f0d
satishkumar <satishsnv@gmail.com>
2017-11-13 00:30:22 +0530

    First assignment

---
+++ module1-solution/app.js
@@ -0,0 +1,39 @@
+(function(){
+  'use strict';
+  angular.module('LunchCheck',[])
+  .controller('LunchCheckController', LunchCheckController);
+  LunchCheckController.$inject = ['$scope'];
+  function LunchCheckController($scope){
+    $scope.items = "";
+    $scope.emptyValues = false;
+    $scope.checkItems = function(){
+      if($scope.items.length > 0){
+        $scope.txtColor = {'color':'green'};
+        $scope.borderColor = {'border-color':'green'};
+        var arrayItems = $scope.items.split(/\s*,\s*/);
+        var itemCount = getItemCount(arrayItems);
+        if(itemCount != arrayItems.length){
+          $scope.emptyValues = true;
+        }
+        if(itemCount <= 3){
+          $scope.comments = "Enjoy!"
+        }else{
+          $scope.comments = "Too much!"
+        }
+      }else {
+        $scope.comments = "Please enter data first";
+        $scope.txtColor = {'color':'red'};
+        $scope.borderColor = {'border-color':'red'};
+      }
+    };
+    function getItemCount(arrayItems){
+      var count = 0;
+      for(let item of arrayItems){
+        if(item.length > 0){
+          count++;
+        }
+      }
+      return count;
+    };
+  }
+})();
